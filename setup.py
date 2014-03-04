0f79c8bd43bf0c6aaa687497a841ec6abe1e6abf
dhahaj <dhahaj@gmail.com>
2014-03-04 10:14:54 -0600

    first commit

--- setup.py
+++ setup.py
@@ -4,9 +4,7 @@ from distutils.core import setup
 
 setup(name='mytools',
 	version='1.0',
-	description='A library containing commone use tools.',
-	author='Dman',
-	author_email='dhahaj@gmail.com',
-	url='https://github.com/py/commontools',
+	description='A library for common tools',
+	url='https://github.com/py/mytools',
 	py_modules=['mytools'],
 	)
